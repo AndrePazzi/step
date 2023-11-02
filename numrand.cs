b1c4e4875cbecc41f6ec8ec94db7323c12eb6dbf
AndrePazzi <149030295+AndrePazzi@users.noreply.github.com>
2023-11-01 20:18:19 -0700

    Add files via upload

---
+++ numrand.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class numrand {
+    public static void Main (string[] args) {
+        int[] nums = new int[10];
+        Random rand  = new Random();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = rand.Next(1, 100);
+            Console.WriteLine(nums[i]);
+        }
+    }
+}
